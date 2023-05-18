fe88ff638c1cadb6d1485d368e1efda11211c6d4
Gonzalo Rodriguez <gonza.private@hotmail.com>
2023-05-18 14:45:50 -0300

    Add files via upload

---
+++ _mysql.php
@@ -0,0 +1,4 @@
+<?php 
+//header('Content-Type: text/html; charset=UTF-8'); // Config - CHARSET
+$arts = mysqli_connect("localhost", "root", "", "pagina_1") or die("No se pudo conectar a MySQL.");
+?>
