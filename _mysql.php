be4599a184b63082b9401672a19974ce7e2b40de
Gonzalo Rodriguez <gonza.private@hotmail.com>
2023-05-18 15:43:30 -0300

    Delete _mysql.php

--- _mysql.php
+++
@@ -1,4 +0,0 @@
-<?php 
-//header('Content-Type: text/html; charset=UTF-8'); // Config - CHARSET
-$arts = mysqli_connect("localhost", "root", "", "pagina_1") or die("No se pudo conectar a MySQL.");
-?>
